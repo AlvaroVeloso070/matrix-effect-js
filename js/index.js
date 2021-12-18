d970929616e492b28c216722280dae18d7575558
AlvaroVeloso070 <alvaroveloso11@gmail.com>
2021-12-18 04:50:41 -0300

    renomeando-variáveis

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
 const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+const contexto = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numeros = '0123456789';
 
-const alphabet = katakana + latin + nums;
+const alfabeto = katakana + letras + numeros;
 
 const fontSize = 16;
-const columns = canvas.width/fontSize;
+const colunas = canvas.width/fontSize;
 
 const rainDrops = [];
 
-for( let x = 0; x < columns; x++ ) {
+for( let x = 0; x < colunas; x++ ) {
 	rainDrops[x] = 1;
 }
 
 const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
+	contexto.fillStyle = 'rgba(0, 0, 0, 0.075)';
+	contexto.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#0F0';
-	context.font = fontSize + 'px monospace';
+	contexto.fillStyle = '#0F0';
+	contexto.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
 	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		const text = alfabeto.charAt(Math.floor(Math.random() * alfabeto.length));
+		contexto.fillText(text, i*fontSize, rainDrops[i]*fontSize);
 		
 		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
 			rainDrops[i] = 0;
